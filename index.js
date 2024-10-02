679c16c3dac8c310ac0462cb5d2aac0b2cf2d00a
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-03 00:12:11 +0600

    Added some dummy data in js file

---
+++ index.js
@@ -0,0 +1,36 @@
+// dummy data
+
+const posts = [
+
+
+  {
+    title : "this is title-1",
+    body : "this is body-1"
+  },
+  {
+    title : "this is title-2",
+    body : "this is body-2"
+  },
+  {
+    title : "this is title-3",
+    body : "this is body-3"
+  },
+  {
+    title : "this is title-4",
+    body : "this is body-4"
+  }
+
+]
+
+{/* <div class="post">
+      <h4 class="post-title">post title 1</h4>
+      <p class="post-body">post description 1</p>
+    </div> */}
+
+// selection 
+
+const postElement = document.querySelector('.posts');
+
+const loadAllData = () => {
+  p
+}
