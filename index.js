53104012e61875da7ea91bba11156aeef4c9b873
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-03 02:01:28 +0600

    add frontend data using js
    without using html here just set frontend data using js

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
 // dummy data
 
 const posts = [
-
-
   {
-    title : "this is title-1",
-    body : "this is body-1"
+    title: "this is title-1",
+    body: "this is body-1",
   },
   {
-    title : "this is title-2",
-    body : "this is body-2"
+    title: "this is title-2",
+    body: "this is body-2",
   },
   {
-    title : "this is title-3",
-    body : "this is body-3"
+    title: "this is title-3",
+    body: "this is body-3",
   },
   {
-    title : "this is title-4",
-    body : "this is body-4"
-  }
+    title: "this is title-4",
+    body: "this is body-4",
+  },
+  {
+    title: "this is title-5",
+    body: "this is body-5",
+  },
+  {
+    title: "this is title-6",
+    body: "this is body-6",
+  },
+  {
+    title: "this is title-7",
+    body: "this is body-7",
+  },
+];
 
-]
-
-{/* <div class="post">
+{
+  /* <div class="post">
       <h4 class="post-title">post title 1</h4>
       <p class="post-body">post description 1</p>
-    </div> */}
+    </div> */
+}
 
-// selection 
+// selection
 
-const postElement = document.querySelector('.posts');
+const postsElement = document.querySelector(".posts");
 
 const loadAllData = () => {
-  p
-}
+  posts.map((post) => {
+    // console.log(post);
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+    <h4 class="post-title">post title 1</h4>
+    <p class="post-body">post description 1</p>
+    `;
+    postsElement.appendChild(postElement)
+  });
+  
+};
+
+loadAllData();
